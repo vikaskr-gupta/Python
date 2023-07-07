f06504f461b2686069984666a0ba3628f927d5cf
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-07-08 03:12:51 +0530

    Create 1_Variable.py

---
+++ 2 Variables and Datatype/1_Variable.py
@@ -0,0 +1,33 @@
+# These are data types...
+
+a = '''Vikas is a
+good boy'''                   #String
+b = 'Vikas Kumar Gupta'       #String 
+c = "Vikas"                   #String
+d = "546456"                  #String 
+e = 76                        #Integer
+f = 76.02                     #Float
+g = True                      #Booleon
+h = None                      #None
+
+#Printing the Variables...
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+#Printing the Type of Vsriables...
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
