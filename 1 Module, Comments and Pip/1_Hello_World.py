021e90c86519b2e19ca20a527c0a953b2a7ce098
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-07-07 21:29:02 +0530

    Create 1_Hello_World.py

---
+++ 1 Module, Comments and Pip/1_Hello_World.py
@@ -0,0 +1,13 @@
+#This is One line comment
+# Aurthor: Vikas
+# Licence to ABC Company...
+
+import os         #Build in Module
+# import flask    #External Module
+
+'''We are thankfull to you,
+   for watching my video.
+   Thankyou So Much'''   #This is multi-line comment
+
+print("Hello! World")
+print("Congratulations For Building Your First Programme")
