a3c6d3f2909ced6c64b106f81bd77078aeaa94f6
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-07-07 21:30:00 +0530

    Create 2_Problem_1_Twinkle.py

---
+++ 1 Module, Comments and Pip/2_Problem_1_Twinkle.py
@@ -0,0 +1,8 @@
+'''
+Write a programme,
+to print "Twinkle Twinkle Little Star" poem in Python
+'''
+print('''Twinkle, twinkle little star
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
