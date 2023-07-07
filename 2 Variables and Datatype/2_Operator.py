9b6bcf43687f01ee33c5fdd7d30f7d5c97f9c0d5
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-07-08 03:13:18 +0530

    Create 2_Operator.py

---
+++ 2 Variables and Datatype/2_Operator.py
@@ -0,0 +1,77 @@
+a = 76
+b = 34
+
+# Arithematic Operators
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+# Assignment Operators
+
+a = 76
+print(a)
+
+b = 76
+b += 2
+print(b)
+
+c = 76
+c += 2
+c -= 4
+print(c)
+
+d = 76
+d += 2
+d -= 4
+d *= 3
+print(d)
+
+e = 76
+e += 2
+e -= 4
+e *= 3
+e /= 2
+print(e)
+
+# Comparision Operator
+a = 76 > 34
+b = 76 < 34
+c = 76 >= 34
+d = 76 <= 34
+e = 76 == 34
+f = 76 == 76
+g = 76 != 34
+h = 76 != 76
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+# Logical Operators
+
+bool1 = True
+bool2 = False
+
+# AND Operator
+print("The value of bool1 and bool2 is", bool1 and bool2)
+print("The value of bool2 and bool1 is", bool2 and bool1)
+print("The value of bool2 and bool2 is", bool2 and bool2)
+print("The value of bool1 and bool1 is", bool1 and bool1)
+
+# OR Operator
+print("The value of bool1 or bool2 is", bool1 or bool2)
+print("The value of bool2 or bool1 is", bool2 or bool1)
+print("The value of bool2 or bool2 is", bool2 or bool2)
+print("The value of bool1 or bool1 is", bool1 or bool1)
+
+# NOT Operator
+print("The value of bool1 is", bool1)
+print("The value of bool2 is", bool2)
+print("The value of not bool1 is", not bool1)
+print("The value of not bool2 is", not bool2)
