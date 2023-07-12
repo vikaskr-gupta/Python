74d2c6b2a4dffef164fa9778fd1901dcdb2a29cc
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-07-12 17:30:37 +0530

    Create 9_Problem_05_avarage.py

---
+++ 2 Variables and Datatype/9_Problem_05_avarage.py
@@ -0,0 +1,8 @@
+a = input("Enter First Number : ")
+b = input("Enter Second Number : ")
+
+c = int(a)
+d = int(b)
+
+#print("The Avarage of a and b is", (a + b)/2)     # Invalid Resign: a and b is in string form
+print("The avarage of c and d is", (c + d)/2)
