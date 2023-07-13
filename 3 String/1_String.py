2c5077ae0ab2a98d1d4485ff4e461632aebe55af
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-07-13 15:12:27 +0530

    Create 1_String.py

---
+++ 3 String/1_String.py
@@ -0,0 +1,5 @@
+# Uses of Single, Doubble and Tripple Qoute
+
+print("This is Vikas' Laptop.")
+print('His Name is "Mr.Vikas".')
+print('''His name is "Mr.Vikas" and his Dog's name is Rambilash.''')
