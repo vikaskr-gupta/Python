879b32f5573610cc1d4cec74c99003ec0a8aecc1
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-07-16 11:23:49 +0530

    Create 1_Lists.py

---
+++ 4 Lists and Tupples/1_Lists.py
@@ -0,0 +1,16 @@
+# Create a list using []
+a = [2, 4, 6, 34, 76, 176]
+
+# Print the value using ()
+print(a)
+
+# Access in the list using a[0], a[1], a[2], a[3]...so on
+print(a[4])
+
+# Change the value of list using...
+a[3] = 42
+print(a)
+
+# We can creat a list by using different types
+b = [76, "Vikas", True, 76.02]
+print(a)
