a7a2a74e0ee433d1e90de7d65257f952c04a6d79
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-07-17 17:04:26 +0530

    Create 3_Lists_methods.py

---
+++ 4 Lists and Tupples/3_Lists_methods.py
@@ -0,0 +1,29 @@
+l1 = [1, 5, 11, 7, 76, 34]
+print(l1)
+
+# ****************************************
+# Shorts the list
+# l1.sort()
+
+# ****************************************
+# Reverse the list
+# l1.reverse()              
+
+# ****************************************
+# Adds 45 at the end of the list
+# l1.append(45)             
+
+# ****************************************
+# Inserts 777 at the index 2
+# l1.insert(2, 777)     
+
+# ****************************************
+# Removes elements at Index 2
+# l1.pop(3)                
+
+# ****************************************
+# Remove 7 form the list
+# l1.remove(7)              
+
+# ****************************************
+print(l1)    #First uncomment then Run the code
