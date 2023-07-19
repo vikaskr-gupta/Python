31ada7d61ac571be27e612fbbe6cea0a3aadda0e
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-07-19 10:39:55 +0530

    Create 4_Tupples.py

---
+++ 4 Lists and Tupples/4_Tupples.py
@@ -0,0 +1,32 @@
+# Creating a tupple Using ()
+tpl = (2, 5, 8, 9, 76, 45)
+print(tpl)
+
+
+# ************************************
+# Creating an Empty Tupple Using ()
+# tpl = ()        # Empty Tupple
+# print(tpl)
+
+
+# ************************************
+# Wrong way to create a Tupple with Single element 
+# tpl = (1)
+# print(tpl)
+
+
+# ************************************
+# Creating a Tupple with Single element
+# tpl = (76,)
+# print(tpl)
+
+
+# ************************************
+# Printing the element of a Tupple...
+# print(tpl[2])
+
+
+# *************************************
+# We can't change or remopve the value of a tuopple
+# tpl[2] = 33      # It is immutable(Unchangable)
+# print(tpl)       #Through an Error!!!
