d4e69b1eb60bd23ba6f8c0ab5e3c503d4b041070
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-07-21 01:00:59 +0530

    Create 6_Problem_01_fruit_list.py

---
+++ 4 Lists and Tupples/6_Problem_01_fruit_list.py
@@ -0,0 +1,10 @@
+f1 = input("Enter First Fruit Name : ")
+f2 = input("Enter Second Fruit Name : ")
+f3 = input("Enter Third Fruit Name : ")
+f4 = input("Enter Fourth Fruit Name : ")
+f5 = input("Enter Fifth Fruit Name : ")
+f6 = input("Enter Sixth Fruit Name : ")
+f7 = input("Enter Seventh Fruit Name : ")
+
+MyFruitName = [f1, f2, f3, f4, f5, f6, f7]
+print(MyFruitName)
