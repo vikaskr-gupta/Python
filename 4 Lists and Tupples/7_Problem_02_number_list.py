53e2152bec1e14f6053f889cc6d5c1c9723075c0
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-07-22 00:10:26 +0530

    Create 7_Problem_02_number_list.py

---
+++ 4 Lists and Tupples/7_Problem_02_number_list.py
@@ -0,0 +1,10 @@
+m1 = int(input("Enter Marks of Student First : "))
+m2 = int(input("Enter Marks of Student Second : "))
+m3 = int(input("Enter Marks of Student Third : "))
+m4 = int(input("Enter Marks of Student Fourth : "))
+m5 = int(input("Enter Marks of Student Fifth : "))
+m6 = int(input("Enter Marks of Student Sixth : "))
+
+StudentsMarks = [m1, m2, m3, m4, m5, m6]
+StudentsMarks.sort()
+print(StudentsMarks)
