b1f6c6fad6bdbfbcc907fb59395f097de8ea98b6
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-08-05 21:47:49 +0530

    Create 3_Sets.py

---
+++ 5 Dictionary and Sets/3_Sets.py
@@ -0,0 +1,7 @@
+a = {1, 3, 4, 8, 10, 76}                   # Repeation is not Allowed
+print(type(a))
+print(a)
+
+b = {1, 3, 12, 4, 43, 1, 8, 12, 10, 76}    # Repeation is not Allowed
+print(type(b))
+print(a)
