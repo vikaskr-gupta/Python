32831fcf1b031409ebe7254f9422f002820df2cf
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-08-06 23:48:12 +0530

    Create 4_Empty_sets.py

---
+++ 5 Dictionary and Sets/4_Empty_sets.py
@@ -0,0 +1,7 @@
+# Important: This syntex will create an empty dictionary, not an empty set.
+a = {}
+print(type(a))
+
+# An empty set can be crated by using the below syntex:
+b = set()
+print(type(b))
