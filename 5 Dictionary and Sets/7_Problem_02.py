729635e6b9d705cd83596e70df68b9485d653efb
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-08-09 07:15:21 +0530

    Create 7_Problem_02.py

---
+++ 5 Dictionary and Sets/7_Problem_02.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+num3 = int(input("Enter Third Number : "))
+num4 = int(input("Enter Fourth Number : "))
+num5 = int(input("Enter Fifth Number : "))
+num6 = int(input("Enter Sixth Number : "))
+num7 = int(input("Enter Seventh Number : "))
+num8 = int(input("Enter Eighth Number : "))
+
+set = {num1, num2, num3, num4, num5, num6, num7, num8}
+print(set)
