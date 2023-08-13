ce42c9413260e2baaf45e71248d05764d18e2b7e
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-08-14 00:07:38 +0530

    Create 94_Problem_07.py

---
+++ 5 Dictionary and Sets/94_Problem_07.py
@@ -0,0 +1,13 @@
+favLang = {}
+
+a = input("Enter your favorate language Rahul : \n")
+b = input("Enter your favorate language Manish : \n")
+c = input("Enter your favorate language Avanish : \n")
+d = input("Enter your favorate language Rahul : \n")
+
+favLang['Rahul'] = a
+favLang['Manish'] = b
+favLang['Avanish'] = c
+favLang['Rahul'] = d      # Last Value is the printed value
+
+print(favLang)
