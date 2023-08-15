26914ab2a8bc7d74b9325817266a3aaf17052849
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-08-16 01:21:21 +0530

    Create 96_Problem_09.py

---
+++ 5 Dictionary and Sets/96_Problem_09.py
@@ -0,0 +1,8 @@
+S = {8, 7, 12, "Harry", [1, 2]}
+print(S)
+
+# We can not store a List in the Set
+# Set is Hashable(Immutable(Unchangable)) but List is Unhashable(Mutable(Changable))
+# Hence Question is Wrong
+
+# Rule: All immutable objects are hashable but not all hashable objects are immutable
