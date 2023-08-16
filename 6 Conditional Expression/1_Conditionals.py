8aa3127dd3fa044022fb21bd99265c48a8f40f56
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-08-17 02:10:40 +0530

    Create 1_Conditionals.py

---
+++ 6 Conditional Expression/1_Conditionals.py
@@ -0,0 +1,17 @@
+# If-elif-else
+
+a = int(input("Enter a Number : \n"))
+
+if(a > 0):
+    print("This is Positive Number")
+elif(a < 0):
+    print("This is Negative Number")
+else:                                # Else is Optional
+    print("This is Whole Number")
+    
+# Code Example:
+a = 22
+if(a > 9):
+    print("Greater")
+else:
+    print("Lesser")
