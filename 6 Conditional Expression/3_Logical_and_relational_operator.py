1179b37ff30093231644bb81f37de872ecb8d26a
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-08-19 00:19:07 +0530

    Create 3_Logical_and_relational_operator.py

---
+++ 6 Conditional Expression/3_Logical_and_relational_operator.py
@@ -0,0 +1,26 @@
+# ***************AND Operator***************************
+a = int(input("Enter your Age : \n"))
+
+if(a > 20 and a < 35):
+    print("You are elligible to work with us. Thank You!")
+else:
+    print("Sorry! You are not elligible for work with us.")
+
+
+# **************OR Operator******************************
+a = int(input("Enter your Age : \n"))
+
+if(a < 20 or a > 35):
+    print("You are elligible to work with us. Thank You!")
+else:
+    print("Sorry! You are not elligible for work with us.")
+
+
+
+# ***************NOT Operator****************************
+a = int(input("Enter Your Age : \n"))
+
+if(a != 18):
+    print("He is Less Than or Greater Than 18 Years Old")
+else:
+    print("He is Now 18 Years Old")
