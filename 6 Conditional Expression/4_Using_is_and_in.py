4b2ac99785f52f7c30f5b6c97055497d07ce2831
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-08-24 20:21:48 +0530

    Create 4_Using_is_and_in.py

---
+++ 6 Conditional Expression/4_Using_is_and_in.py
@@ -0,0 +1,16 @@
+# *****************Uses of IS*********************
+a = int(input("Enter A Number : \n"))
+
+if(a is 45):                      # Here is Represent to ==
+    print("It is 45")
+else:
+    print("It is not 45")
+
+
+# *****************Uses of IN*********************
+a = [2, 4, 6, 8, 10]
+
+if(5 in a):
+    print("It is in the list")
+else:
+    print("It is not in the list")
