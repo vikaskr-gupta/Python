e4bcc7907bfc8f7cd64688202d43eb0900671767
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-09-01 13:46:09 +0530

    Create 1_While_loop.py

---
+++ 7 Loops/1_While_loop.py
@@ -0,0 +1,13 @@
+i = 0
+while(i < 10):
+    print("Yes" + str(i))
+    i = i + 1
+
+#**************************************
+
+
+# i = 0
+# while(i < 10):         # Here, condition is always true and it uses for infinite loop
+#     print("Yes" + str(i))
+
+# It printed infinite times Yes because i is always less than 10
