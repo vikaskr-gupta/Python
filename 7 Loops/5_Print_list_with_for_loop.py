12bc1abab92a493810fef21f67df75f7ca9101b2
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-09-02 23:25:56 +0530

    Create 5_Print_list_with_for_loop.py

---
+++ 7 Loops/5_Print_list_with_for_loop.py
@@ -0,0 +1,14 @@
+# Uses of for loop is very easy compare to while loop
+
+fruits = ["1", "7", "9", "21"]
+
+for item in fruits:
+    print(item)
+
+
+#***************************************************
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+for item in fruits:
+    print(item)
