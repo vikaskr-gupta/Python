68c7e967e15ca159cac4e5d4fe9d86762e2e183a
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-09-02 23:26:50 +0530

    Create 6_range_function_with_for_loop.py

---
+++ 7 Loops/6_range_function_with_for_loop.py
@@ -0,0 +1,22 @@
+for i in range(8):
+    print(i)       # It printed 0 to 7(By default it set to be start to 0)
+# ********************************************
+
+
+for i in range(1, 8):
+    print(i)       # It printed 1 to 7(strat, stop, step-size). By default step-size is set to be 1
+#*********************************************
+
+
+for i in range(10, 21):
+    print(i)       # It printed 10 to 20
+#*********************************************
+
+
+for i in range(1, 8, 2):
+    print(i)       # It printed 1 to 8 with the difference 2(start, stop, Step-size)
+#*********************************************
+
+
+for i in range(1, 20, 3):
+    print(i)       # It printed 1 to 20 with the difference 3(start, stop, Step-size)
