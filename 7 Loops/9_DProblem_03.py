5f10a126ba690f06edb0dacca906bdb2512d12df
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-09-04 12:56:45 +0530

    Create 9_DProblem_03.py

---
+++ 7 Loops/9_DProblem_03.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a Number "))
+
+i = 1
+while (i < 11 and i > 0):
+    
+    # print(str(num) + "X" + str(i) + "=" + str(num*i))
+    print(f"{num} X {i} = {num * i}")
+    i = i + 1
