e6c26bfb167c0f0f12677b30013839aa81f113cc
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-09-05 11:10:14 +0530

    Create 9F_Problem_05.py

---
+++ 7 Loops/9F_Problem_05.py
@@ -0,0 +1,11 @@
+# Trying, But not able to make it using while loop...
+
+# Using For Loop
+num = int(input("Enter a Number... "))
+
+sum = 1
+for i in range(1, num + 1):
+    sum = sum + i
+
+sum = sum - 1
+print(f"The Sum of {num} Natual Numbers is... {sum}")
