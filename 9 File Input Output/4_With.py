02f3dc222f82e08d6cf2d4f67d6f1c7dd1a703b2
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-09-09 21:40:11 +0530

    Create 4_With.py

---
+++ 9 File Input Output/4_With.py
@@ -0,0 +1,15 @@
+# DON'T NEED TO WRITE f.colse() AS IT IS DONE AUTOMATICALLY HERE...
+
+
+with open('third_sample.txt', 'r') as f:
+    a = f.read()
+
+    print(a)
+#*****************************************
+
+
+
+# with open('fourth_sample.txt', 'w') as f:
+#     a = f.write("we are not writing YET.")
+
+#     print(a)
